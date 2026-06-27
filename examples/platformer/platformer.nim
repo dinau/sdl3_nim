763132b2b18f65d6532bb059dbbedcfbeff71b2c
Denise <denise_amiga@hotmail.com>
2026-06-27 14:49:45 +0200

    Fixed examples. SDL3 automatically manages the context.

--- examples/platformer/platformer.nim
+++ examples/platformer/platformer.nim
@@ -436,10 +436,10 @@ proc main() =
   #-----------------------------
   #--- Setting OpenGL3 backend
   #-----------------------------
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0)
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE.cint)
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3)
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3)
+  # SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0)
+  # SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE.cint)
+  # SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3)
+  # SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3)
 
   #----------------------
   #--- Create SDL window
